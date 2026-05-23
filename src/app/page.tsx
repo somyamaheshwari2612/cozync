3e077ae13c6f2f0846c5ab98ce9e23c35bcbcbe8
Somya Maheshwari <somyamaheshwari2006@gmail.com>
2026-05-23 09:04:50 +0530

    fix: serve calendar at root URL, remove /calendar from address bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import CalendarPage from "@/app/calendar/page";
 
 export default function Home() {
-  redirect("/calendar");
+  return <CalendarPage />;
 }
